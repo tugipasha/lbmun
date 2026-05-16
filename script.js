792d1bf3ca4428273e5f70c26a1e571473446696
Tolga <aydintolga008@gmail.com>
2026-05-16 15:03:54 +0300

    Update Google Apps Script URL in script.js

--- script.js
+++ script.js
@@ -501,7 +501,7 @@
       console.log('Submitting application data:', Object.fromEntries(searchParams));
 
       // Google Apps Script URL
-      const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzjEYGOlALTw9w28L-lnwiZmsGMAi0Zng07nScRGIKNZcIc9x3IoKTegNQOHvbctTDA/exec';
+      const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwDs3creewKOGbZ6m1j2t89iXEnYPCiE3Dc6zj3hdnaAddgu5-zNf4qyap3oYG97v3UbQ/exec';
 
       // Send data to Google Sheets
       // Note: Google Apps Script requires no-cors mode for cross-origin requests
